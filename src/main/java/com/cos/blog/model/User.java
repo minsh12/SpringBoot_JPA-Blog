bcda12aefe925fdbca0a9bbe1a5461b2248b51df
minsh12 <tlsalsgpp12@gmail.com>
2022-04-08 15:53:01 +0900

    DummyControllerTest

--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -2,11 +2,7 @@ package com.cos.blog.model;
 
 import java.sql.Timestamp;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,17 +10,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Entity // User 클래스가 MySQL 테이블에 생성된다.
+@Entity // User 클래스가 MySQL 테이블에 생성됨
         // ORM - JAVA의 Object를 테이블로 매핑해주는 기술
+//@DynamicInsert      // insert할때 null 인 필드 제외시켜줌
 public class User {
 
     @Id // Primary key
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 제작된 DB의 넘버링을 따라간다.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 제작된 DB의 넘버링을 따라감
     private int id; // 시퀀스, auto_increment
 
 
@@ -38,9 +36,9 @@ public class User {
     @Column(nullable = false, length = 50)
     private String email;
 
-
-    @ColumnDefault("'user'") // 디폴트값으로 user가 저장됨. 양 옆으로 작은 따옴표를 줘서 문자라는 것을 알려줘야함
-    private String role; // Enum을 쓰는게 좋다. // admin, user, manager -> 도메인(프로그래밍에서 도메인은 범위를 의미)
+    //@ColumnDefault("'user'") // 디폴트값으로 user가 저장됨. 양 옆으로 작은 따옴표를 줘서 문자라는 것을 알려줘야함
+    @Enumerated(EnumType.STRING)    // DB는 RoleType이란게 없음. 이렇게 어노테이션붙여 해당 enum이 String임을 알려줘야함.
+    private RoleType role; // Enum을 쓰는게 좋음. // admin, user, manager -> 도메인(프로그래밍에서 도메인은 범위를 의미)
 
     @CreationTimestamp // 값을 비워두고 insert해도 시간이 자동 입력
     private Timestamp createDate;
--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,16 +1,101 @@
 package com.cos.blog.test;
 
+import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
-@RestController // 페이지이동없이 데이터만 리턴해주며 응답만 해줄 수 있도록
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.function.Supplier;
+
+@RestController     // : html파일이 아니라 data를 리턴해주는 controller
+                    // 페이지이동없이 데이터만 리턴해주며 응답만 해줄 수 있도록
 public class DummyControllerTest {
 
     @Autowired  // 의존성 주입 (DI)
     private UserRepository userRepository;
+
+    // password, email을 수정
+    @Transactional      // save()를 하지 않아도 update가 되게해줌
+    @PutMapping("/dummy/user/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody User requestUser) {   // @ResquestBody로 (Body-raw)json데이터를 받아옴
+                                                                                    // json데이터를 요청 -> Springboot이 java object로 변환해서 받음 (MessageConverter의 Jackson라이브러리가 변환해서 받음)
+        System.out.println("id: " + id);
+        System.out.println("password: " + requestUser.getPassword());
+        System.out.println("email: " + requestUser.getEmail());
+
+        User user = userRepository.findById(id).orElseThrow(()->{
+            return new IllegalArgumentException("수정에 실패하였습니다.");
+        });
+        user.setPassword(requestUser.getPassword());
+        user.setEmail(requestUser.getEmail());
+
+        /* save 함수
+            id를 전달하지 않으면 insert를 하고,
+            id를 전달하면 해당 id에 대한 데이터가 있으면 update를 하고,
+            id를 전달하면 해당 id에 대한 데이터가 없으면 insert를 함.
+        */
+        //userRepository.save(user);
+
+        // 더티 체킹
+        return null;
+    }
+
+    // http://localhost:8000/blog/dummy/user
+    @GetMapping("/dummy/users")
+    public List<User> list() {
+        return userRepository.findAll();
+    }
+
+    // [페이징] 한페이지당 최신순의 id에 대한 2건의 데이터를 리턴받아볼 것
+    // http://localhost:8000/blog/dummy/user   //http://localhost:8000/blog/dummy/user?page=0 (page=1)
+    @GetMapping("/dummy/user")
+    public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<User> pagingUser = userRepository.findAll(pageable);
+
+        List<User> users = pagingUser.getContent();
+        return users;
+    }
+
+    // {id} 주소로 파라메터를 전달받을 수 O
+    // http://localhost:8000/blog/dummy/user/3
+    @GetMapping("/dummy/user/{id}")
+    public User detail(@PathVariable int id) {
+
+        /* findById() 리턴형태가 Optional인 이유 :
+        *   만약 user/9를 찾으면 디비에서 못찾아와서 user = null이 되고,
+        *   그럼 return null이 되서 그럼 프로그램에 문제가 될 수 O
+        *   -> Optional로 User객체를 감싸서 가져올테니 null인지 아닌지 판단해서 return하란 의도 */
+
+        /* 바로 아래 코드를 람다식으로 구현 (Supplier 등 타입을 전혀 몰라도 됨)
+        User user = userRepository.findById(id).orElseThrow(()->{
+            return new IllegalArgumentException("해당 사용자는 없습니다.);
+        });
+        */
+        User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+            @Override
+            public IllegalArgumentException get() {
+                return new IllegalArgumentException("해당 유저는 없습니다. id : " + id);
+            }
+        });
+
+        /*
+            요청은 웹브라우저에서,
+            user 객체 = 자바 오브젝트, 자바 오브젝트를 반환하는데 그전에 변환 필요.
+            웹브라우저가 이해할 수 있는 데이터인 json으로.
+            스프링부트는 MessageConverter가 응답시에 자동 작동.
+            만약에 자바 오브젝트를 리턴하게 되면MessageConverter가 Jackson 라이브러리를 호출해서
+            user 오브젝트를 json으로 변환해서 브러우저에게 전달.
+        */
+
+        return user;
+    }
 
     // http://localhost:8000/blog/dummy/join (요청)
     // http의 body에 username, password, email 데이터를 가지고 요청하면 join()함수의 파리미터에 맞춰 들어감
@@ -29,7 +114,8 @@ public class DummyControllerTest {
         System.out.println("email : " + user.getEmail());
         System.out.println("role : " + user.getCreateDate());
 
-        userRepository.save(user);
+        user.setRole(RoleType.USER);
+        userRepository.save(user);      // insert할 때 사용
         return "회원가입이 완료되었습니다.";
     }
 }
